751e2feeaecccb21fee3fcd08f83e85907967ed5
Philipp Schmitt <philipp@schmitt.co>
2020-03-12 13:57:58 +0100

    Fix non-push builds

--- buildx.sh
+++ buildx.sh
@@ -13,17 +13,15 @@ fi
 
 case "$1" in
   push|p|--push|-p)
-    PUSH_IMAGE=true
+    EXTRA_BUILD_ARGS+=("--push")
     ;;
   *)
-    PUSH_IMAGE=false
     EXTRA_BUILD_ARGS+=("--load")
     ;;
 esac
 
 docker buildx build \
   --platform "linux/amd64,linux/386" \
-  --output "type=image,push=${PUSH_IMAGE}" \
   "${EXTRA_BUILD_ARGS[@]}" \
   -t "$IMAGE" \
   .
